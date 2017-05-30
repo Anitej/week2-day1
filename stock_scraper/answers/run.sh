53fd7411e62382b7f5ddb4993f02ae20d8fa3023
Anitej <aneetej@gmail.com>
2017-05-30 13:27:53 +0530

    Update run.sh

--- stock_scraper/answers/run.sh
+++ stock_scraper/answers/run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-NEW_AGENT="Mozilla/5.0"
-TIMESTAMP=$(date  +%s)
+NEW_AGENT="Mozilla/5.0" #new agent is an abbrev. Mozzila is used as the browser to scrape. Fed to weget.
+TIMESTAMP=$(date  +%s) #time of the current date in seconds format
 
-DL_BZX="http://www.bats.com/us/equities/market_statistics/symbols_traded/csv?mkt=bzx"
+DL_BZX="http://www.bats.com/us/equities/market_statistics/symbols_traded/csv?mkt=bzx" 
 DL_BYX="http://www.bats.com/us/equities/market_statistics/symbols_traded/csv?mkt=byx"
 DL_EDGA="http://www.bats.com/us/equities/market_statistics/symbols_traded/csv?mkt=edga"
 DL_EDGX="http://www.bats.com/us/equities/market_statistics/symbols_traded/csv?mkt=edgx"
@@ -12,23 +12,23 @@ EP_BYX="bats_byx-symbols_traded.csv"
 EP_EDGA="bats_edga-symbols_traded.csv"
 EP_EDGX="bats_edgx-symbols_traded.csv"
 
-wget --user-agent=$NEW_AGENT $DL_BZX -O $EP_BZX
+wget --user-agent=$NEW_AGENT $DL_BZX -O $EP_BZX # --user-agent is a switch. $NEW_AGENT is referring the browser. $DL_BZX -O $EP_BZX outputs from deep link to end point file as csv
 wget --user-agent=$NEW_AGENT $DL_BYX -O $EP_BYX
 wget --user-agent=$NEW_AGENT $DL_EDGA -O $EP_EDGA
 wget --user-agent=$NEW_AGENT $DL_EDGX -O $EP_EDGX
 
-sed -i '/^$/d' $EP_BZX
+sed -i '/^$/d' $EP_BZX #deletes whitespace
 sed -i '/^$/d' $EP_BYX
 sed -i '/^$/d' $EP_EDGA
 sed -i '/^$/d' $EP_EDGX
-sed -i '1d' $EP_BZX
+sed -i '1d' $EP_BZX #delete the csv header (it says 'symbols')
 sed -i '1d' $EP_BYX
 sed -i '1d' $EP_EDGA
 sed -i '1d' $EP_EDGX
 
-mkdir manifest
+mkdir manifest #creates directory manifest
 
-mv $EP_BZX manifest
+mv $EP_BZX manifest #sends csv files to manifest
 mv $EP_BYX manifest
 mv $EP_EDGA manifest
 mv $EP_EDGX manifest
